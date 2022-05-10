9f0589c51d1f314c400376c4eef99653da72d519
Amir <fen1979@users.noreply.github.com>
2022-05-10 20:24:41 +0300

    added menu buttons
    added more menu buttons, created PCB for tests

--- Compressor_Emergency_timer_NTI.ino
+++ Compressor_Emergency_timer_NTI.ino
@@ -1,29 +1,35 @@
-//#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 #define sensorIn A1
 #define relayOut1 A2
 #define relayOut2 A3
-#define relayOut3 A5
-#define relayOut4 A7
+#define relayOut3 6
+#define relayOut4 7
 #define indication 13
-#define incBtn 12
-#define decBtn 11
 
-#define CLEAR 1
-#define NOT_CLEAR 0
+#define saveBtn 8
+#define decBtn 9
+#define menuBtn 10
+#define incBtn 11
+
+#define CLEAR_LCD 1
+#define NO_CLEAR_LCD 0
+
 
 // time for stop the compressor
 int timerResetDate = 10; // minutes
 int timerCount = 0;
 bool writeToLcd = false;
+byte menuPositioner = 0;
 
 LiquidCrystal_I2C lcd(0x3E, 16, 2);
 
 void setup() {
-  pinMode(sensorIn, INPUT_PULLUP);
-  pinMode(incBtn, INPUT_PULLUP);
-  pinMode(decBtn, INPUT_PULLUP);
+  pinMode(sensorIn, INPUT);
+  pinMode(incBtn, INPUT);
+  pinMode(decBtn, INPUT);
+  pinMode(menuBtn, INPUT);
+  pinMode(saveBtn, INPUT);
 
   pinMode(relayOut1, OUTPUT);
   pinMode(relayOut2, OUTPUT);
@@ -31,54 +37,41 @@ void setup() {
   pinMode(relayOut4, OUTPUT);
   pinMode(indication, OUTPUT);
 
-  analogWrite(relayOut1, 1024);
-  analogWrite(relayOut2, 1024);
-  analogWrite(relayOut3, 1024);
-  analogWrite(relayOut4, 1024);
+  analogWrite(relayOut1, 0);
+  analogWrite(relayOut2, 0);
+  analogWrite(relayOut3, 0);
+  analogWrite(relayOut4, 0);
   analogWrite(indication, 1024);
 
   lcd.begin();
   lcd.clear();
   lcd.setCursor(0, 0);
   String outTxt = "Current Emergency stop time is " + String(timerResetDate) + " minute";
-  scrollMessage(0, outTxt, 350, 16);
+  // scrollMessage(0, outTxt, 350, 16);
 }
 
 
 
 void loop() {
+  // menu function
+  isMenuCalled();
 
   if (SensorMesurment()) {
     StartEmergencyTimer();
 
     if (writeToLcd) {
-      WriteStaticTextToLcd(0, "Emergency", 1);
-      WriteStaticTextToLcd(1, "Timer started!!!", 0);
+      WriteStaticTextToLcd(0, "Emergency", CLEAR_LCD);
+      WriteStaticTextToLcd(1, "Timer started!!!", NO_CLEAR_LCD);
       writeToLcd = false;
     }
   } else {
     analogWrite(indication, 1024);
     timerCount = 0;
     if (!writeToLcd) {
-      WriteStaticTextToLcd(0, "Normal Work.", 1);
-      WriteStaticTextToLcd(1, "Stop time " + String(timerResetDate) + " min", 0);
+      WriteStaticTextToLcd(0, "Normal Work.", CLEAR_LCD);
+      WriteStaticTextToLcd(1, "Stop time " + String(timerResetDate) + " min", NO_CLEAR_LCD);
       writeToLcd = true;
     }
-  }
-
-  if (digitalRead(incBtn) == LOW && timerResetDate != 30) {
-    timerResetDate++;
-    WriteStaticTextToLcd(0, "Stop Time", 1);
-    WriteStaticTextToLcd(1, String(timerResetDate) + " min", 0);
-    writeToLcd = false;
-    delay(800);
-  }
-  if (digitalRead(decBtn) == LOW && timerResetDate != 1) {
-    timerResetDate--;
-    WriteStaticTextToLcd(0, "Stop Time", 1);
-    WriteStaticTextToLcd(1, String(timerResetDate) + " min", 0);
-    writeToLcd = false;
-    delay(800);
   }
   delay(10);
 }
@@ -119,6 +112,61 @@ void StartEmergencyTimer() {
     lcd.setCursor(0, 0);
     scrollMessage(0, "For restart need power off !!!", 350, 16);
   }
+}
+
+// menu buttons and options
+void isMenuCalled() {
+
+  if (digitalRead(menuBtn) == LOW) {
+    delay(250);
+    if ( menuPositioner != 4) {
+      menuPositioner++;
+    } else {
+      menuPositioner = 0;
+    }
+  }
+
+  switch (menuPositioner) {
+    // timer inc/dec
+    case 1: {
+        if (digitalRead(incBtn) == LOW || digitalRead(decBtn) == LOW) {
+          WriteStaticTextToLcd(0, String(menuPositioner) + ") Stop Time", CLEAR_LCD);
+          WriteStaticTextToLcd(1, String(timerResetDate) + " min", NO_CLEAR_LCD);
+          if (digitalRead(incBtn) == LOW && timerResetDate != 30) {
+            timerResetDate++;
+            writeToLcd = false;
+            delay(250);
+          }
+
+          if (digitalRead(decBtn) == LOW && timerResetDate != 1) {
+            timerResetDate--;
+            writeToLcd = false;
+            delay(250);
+          }
+        }
+      } break;
+    // stop all works
+    case 2: {
+        WriteStaticTextToLcd(0, String(menuPositioner) + ") Stop Time", CLEAR_LCD);
+        WriteStaticTextToLcd(1, String(timerResetDate) + " min", NO_CLEAR_LCD);
+      } break;
+    //
+    case 3: {
+        WriteStaticTextToLcd(0, String(menuPositioner) + ") Stop Time", CLEAR_LCD);
+        WriteStaticTextToLcd(1, String(timerResetDate) + " min", NO_CLEAR_LCD);
+      } break;
+    //
+    case 4: {
+        WriteStaticTextToLcd(0, String(menuPositioner) + ") Stop Time", CLEAR_LCD);
+        WriteStaticTextToLcd(1, String(timerResetDate) + " min", NO_CLEAR_LCD);
+      } break;
+
+    default : break;
+  }
+
+
+
+
 }
 
 // screen static text
