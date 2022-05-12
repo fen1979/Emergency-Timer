c0a2b876313b488ef74ab511f14ab794d3384b95
Amir <fen1979@users.noreply.github.com>
2022-05-12 21:25:23 +0300

    complitly changed code v3

--- v2-test.ino
+++ v2-test.ino
@@ -10,23 +10,22 @@
 #define incBtn 11
 #define indication 13
 
-#define CLEAR_LCD 1
-#define NO_CLEAR_LCD 0
-
 byte inPin[] = {A1, 8, 9, 10, 11};
 byte outPin[] = {13, A2, A3, 6, 7};
 int timerResetDate = 10; // minutes
 int timerCount = 0;
-bool writeToLcd = false;
 byte menuNow = 1;
+bool writeToLcd = false;
 // for PNP Line
 byte motorStatus = 1;
+int motorPower[4] = {0, 0, 0, 0};
+bool power = false;
 
 LiquidCrystal_I2C lcd(0x3E, 16, 2);
 
 void setup() {
   for (byte i = 0; i < sizeof(inPin); i++) {
-    pinMode(inPin[i], INPUT);
+    pinMode(inPin[i], INPUT_PULLUP);
   }
 
   for (byte i = 0; i < sizeof(outPin); i++) {
@@ -43,27 +42,39 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
   String outTxt = "Emergency stop time is " + String(timerResetDate) + " min";
-  // scrollMessage(0, outTxt, 350, 16);
-}
-
-void loop() {
-  // menu function
-  Menu();
-  // function for emergency stop
-  if (menuNow == 1) {
-    EmergencyCase();
-  }
-  // function for PNP Line
-  if (menuNow == 2) {
-    MotorSwitching();
-  }
-  delay(10);
+  scrollMessage(0, outTxt, 350, 16);
 }
 
 bool SensorMesurment() {
   return (analogRead(sensorIn) < 50) ? true : false;
 }
 
+// EMERGENCY CASE
+void EmergencyCase() {
+  if (SensorMesurment()) {
+    StartEmergencyTimer();
+
+    if (writeToLcd) {
+      PrintTextToLCD("Emergency", "Timer started!!!");
+      writeToLcd = false;
+    }
+  } else {
+    analogWrite(indication, 1024);
+    timerCount = 0;
+    if (!writeToLcd) {
+      PrintTextToLCD("Normal Work.", "Stop time " + String(timerResetDate) + " min");
+      writeToLcd = true;
+    }
+  }
+  // increase/decrease time to stop actions
+  if (digitalRead(incBtn) == LOW || digitalRead(decBtn) == LOW) {
+    PrintTextToLCD("Stop Time", String(timerResetDate) + " min");
+    if (digitalRead(incBtn) == LOW && timerResetDate != 30) timerResetDate++;
+    if (digitalRead(decBtn) == LOW && timerResetDate != 1) timerResetDate--;
+    writeToLcd = false;
+    delay(350);
+  }
+}
 void StartEmergencyTimer() {
   if (timerCount < (timerResetDate * 60)) {
     timerCount++;
@@ -77,8 +88,7 @@ void StartEmergencyTimer() {
     int h = t;
 
     String timeToStop = String(h) + ":" + String(m) + ":" + String(s);
-    WriteStaticTextToLcd(0, "Emergency Timer", CLEAR_LCD);
-    WriteStaticTextToLcd(1, "Stop - " + timeToStop, NO_CLEAR_LCD);
+    PrintTextToLCD("Emergency Timer", "Stop - " + timeToStop);
 
     delay(990);
   } else {
@@ -92,82 +102,49 @@ void StartEmergencyTimer() {
     scrollMessage(0, "For restart need power off !!!", 350, 16);
   }
 }
-void EmergencyCase() {
-  if (SensorMesurment()) {
-    StartEmergencyTimer();
-
-    if (writeToLcd) {
-      WriteStaticTextToLcd(0, "Emergency", CLEAR_LCD);
-      WriteStaticTextToLcd(1, "Timer started!!!", NO_CLEAR_LCD);
-      writeToLcd = false;
-    }
-  } else {
-    analogWrite(indication, 1024);
-    timerCount = 0;
-    if (!writeToLcd) {
-      WriteStaticTextToLcd(0, "Normal Work.", CLEAR_LCD);
-      WriteStaticTextToLcd(1, "Stop time " + String(timerResetDate) + " min", NO_CLEAR_LCD);
-      writeToLcd = true;
-    }
-  }
-}
-
+// PNP LINE CASE
 void MotorSwitching() {
-  analogWrite(indication, 0);
-  if (SensorMesurment()) {
-    motorStatus = (motorStatus == 1) ? 2 : 1;
-  }
-
-  switch (motorStatus) {
-    case 1: {
-        analogWrite(relayOut1, 1024);
-        analogWrite(relayOut2, 0);
-        analogWrite(relayOut3, 1024);
-        analogWrite(relayOut4, 0);
-      } break;
-    case 2: {
-        analogWrite(relayOut1, 0);
-        analogWrite(relayOut2, 1024);
-        analogWrite(relayOut3, 0);
-        analogWrite(relayOut4, 1024);
-      } break;
-  }
-}
-
-// menu buttons and options
-void Menu() {
-  if (digitalRead(menuBtn) == LOW) {
-    menuNow = (menuNow == 1) ? 2 : 1;
-    delay(500);
-  }
-
-  if (digitalRead(incBtn) == LOW || digitalRead(decBtn) == LOW) {
-    // increase/decrease time to stop
-    WriteStaticTextToLcd(0, "Stop Time", CLEAR_LCD);
-    WriteStaticTextToLcd(1, String(timerResetDate) + " min", NO_CLEAR_LCD);
-
-    if (digitalRead(incBtn) == LOW && timerResetDate != 30) {
-      timerResetDate++;
-    }
-    if (digitalRead(decBtn) == LOW && timerResetDate != 1) {
-      timerResetDate--;
-    }
-
+  if (writeToLcd) {
+    PrintTextToLCD("PNP Line ON", "");
     writeToLcd = false;
-    delay(250);
   }
-}
-
-// screen static text
-void WriteStaticTextToLcd(int row, String message, byte clearOrNot) {
-  if (clearOrNot == 1) {
-    lcd.clear();
+  if (SensorMesurment() && motorStatus == 1 && !power) {
+    PrintTextToLCD("PNP Line ON", "Motor 1,3 ON");
+    power = true;
+    motorPower[0] = motorPower[2] = 1024;
   }
-  lcd.setCursor(0, row);
-  lcd.print(message);
+  if (!SensorMesurment() && motorStatus == 1 && power) {
+    PrintTextToLCD("PNP Line ON", "Motor 1,3 OFF");
+    power = false;
+    motorStatus = 2;
+    motorPower[0] = motorPower[2] = 0;
+  }
+  if (SensorMesurment() && motorStatus == 2 && !power) {
+    PrintTextToLCD("PNP Line ON", "Motor 2,4 ON");
+    power = true;
+    motorPower[1] = motorPower[3] = 1024;
+  }
+  if (!SensorMesurment() && motorStatus == 2 && power) {
+    PrintTextToLCD("PNP Line ON", "Motor 2,4 OFF");
+    power = false;
+    motorStatus = 1;
+    motorPower[1] = motorPower[3] = 0;
+  }
+  // changing pin state
+  analogWrite(relayOut1, motorPower[0]);
+  analogWrite(relayOut2, motorPower[1]);
+  analogWrite(relayOut3, motorPower[2]);
+  analogWrite(relayOut4, motorPower[3]);
 }
-
-// screen scrolling message function
+// screen line by line print text case
+void PrintTextToLCD(String topRow, String bottomRow) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(topRow);
+  lcd.setCursor(0, 1);
+  lcd.print(bottomRow);
+}
+// screen scrolling message case
 void scrollMessage(int row, String message, int delayTime, int totalColumns) {
   for (int i = 0; i < totalColumns; i++) {
     message = " " + message;
@@ -178,4 +155,21 @@ void scrollMessage(int row, String message, int delayTime, int totalColumns) {
     lcd.print(message.substring(position, position + totalColumns));
     delay(delayTime);
   }
+}
+
+void loop() {
+  // menu round cicle
+  if (digitalRead(menuBtn) == LOW) {
+    if (menuNow != 3)menuNow++;
+    else menuNow = 1;
+    delay(350);
+  }
+
+  // work code calls
+  if (menuNow == 1) EmergencyCase();
+  if (menuNow == 2) MotorSwitching();
+  if (menuNow == 3) PrintTextToLCD("menu 3 top line", "bottom line");
+  // save btn actions
+  // if (digitalRead(saveBtn) == LOW){}
+  delay(10);
 }
